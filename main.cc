0c0dc76afd80c969383d55ec9ac01c2fa7b47204
y0ub <y0_@y0.local>
2024-02-06 21:33:48 +0900

    update createInfo proc.

--- main.cc
+++ main.cc
@@ -17,9 +17,10 @@ class HelloTriangleApplication
     }
   private:
     // values
-    GLFWwindow* window;
     const uint32_t kwidth = 800;
     const uint32_t kheight = 600;
+    GLFWwindow* window;
+    VkInstance instance;
 
     // functions
     void initWindow()
@@ -33,6 +34,48 @@ class HelloTriangleApplication
 
     void initVulkan()
     {
+      createInstance();
+    }
+
+    void createInstance()
+    {
+      VkApplicationInfo appInfo{};
+      appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+      appInfo.pApplicationName = "Hello Triangle";
+      appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+      appInfo.pEngineName = "No Engine";
+      appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+      appInfo.apiVersion = VK_API_VERSION_1_0;
+
+      VkInstanceCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+      createInfo.pApplicationInfo = &appInfo;
+
+      uint32_t glfwExtensionCount = 0;
+      const char** glfwExtensions;
+
+      glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+      std::vector<const char*> requiredGlfwExtensions;
+
+      for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+        requiredGlfwExtensions.emplace_back(glfwExtensions[i]);
+      }
+
+      requiredGlfwExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+      createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+
+      createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredGlfwExtensions.size());
+      createInfo.ppEnabledExtensionNames = requiredGlfwExtensions.data();
+
+      createInfo.enabledLayerCount = 0;
+
+      if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!!!!11!");
+      }
+
 
     }
 
