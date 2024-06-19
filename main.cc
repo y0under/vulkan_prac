076a074195fc7ab6295f9bad42296758013bd4e4
y0ub <y0lit7515@gmail.com>
2024-06-19 21:41:16 +0900

    update: add function chooseSwapSurfaceFormat

--- main.cc
+++ main.cc
@@ -236,6 +236,16 @@ class HelloTriangleApplication
       return details;
     }
 
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+      for (const auto& availableFormat : availableFormats) {
+        if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+          return availableFormat;
+        }
+      }
+
+      return availableFormats[0];
+    }
+
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
     {
       uint32_t queueFamilyCount = 0;
