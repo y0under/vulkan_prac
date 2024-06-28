57863ca6b18d852f28346db52ea8761803a75733
y0ub <y0lit7515@gmail.com>
2024-06-28 19:24:59 +0900

    update: add function createShaderModule

--- main.cc
+++ main.cc
@@ -406,6 +406,28 @@ class HelloTriangleApplication {
       auto fragShaderCode = ReadFile("shader/frag.spv");
     }
 
+
+    /**
+     * @brief 
+     *
+     * @param code
+     *
+     * @return 
+     */
+    VkShaderModule createShaderModule(const std::vector<char> &code) {
+      VkShaderModuleCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+      createInfo.codeSize = code.size();
+      createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+      VkShaderModule shaderModule;
+      if (vkCreateShaderModule(device_, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create shader module!");
+      }
+
+      return shaderModule;
+    }
+
     /**
      * @brief quering details of swap chain support
      *
